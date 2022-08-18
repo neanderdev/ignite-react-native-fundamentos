32e03b7826e7bbb5e7d6b7f47ebd6a4f71221db7
neanderdev <neander.faria@hotmail.com>
2022-08-18 09:35:33 -0300

    Finalizing the test

--- e2e/firstTest.e2e.js
+++ e2e/firstTest.e2e.js
@@ -14,10 +14,15 @@ describe('Meus primeiros testes', () => {
   it('check register a new skill', async () => {
     const inputNewSkill = await element(by.id('input-new'));
     const buttonAdd = await element(by.id('button-add'));
+    const flatListSkills = await element(by.id('flat-list-skills'));
 
     await inputNewSkill.tap();
     await inputNewSkill.typeText('React Native');
 
     await buttonAdd.tap();
+
+    await flatListSkills.tap();
+
+    expect(element(by.id('flat-list-skills'))).toBeVisible();
   });
 });
