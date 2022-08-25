90ffc405f523c77bdab1f9dc50afb3508053a8be
neanderdev <neander.faria@hotmail.com>
2022-08-25 14:15:27 -0300

    Rastreando erros em produção

--- App.tsx
+++ App.tsx
@@ -2,11 +2,19 @@ import React, { useEffect } from 'react';
 import { StatusBar } from 'react-native';
 import SplashScreen from 'react-native-splash-screen';
 import codePush from 'react-native-code-push';
+import * as Sentry from "@sentry/react-native";
+
+Sentry.init({
+  dsn: "https://b86cd4e4af4f49fd9df62309d60e8cd1@o1306210.ingest.sentry.io/6687965",
+  enableNative: false,
+});
 
 import { Home } from './src/pages/Home';
 
 function App() {
   useEffect(() => {
+    // throw new Error('Teste para error tracking sentry');
+
     codePush.sync({
       installMode: codePush.InstallMode.IMMEDIATE,
     });
